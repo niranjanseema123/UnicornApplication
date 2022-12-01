d2eaf1ab1349587dd15cc2588507cfe3fe416216
Niranjan <niranjanseema@hotmail.com>
2022-12-01 17:10:36 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId:         'us-west-1_P7aYVBPME',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '6vrh0dt4otssaffra48hpveufq',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId:   '1rv1j4olu656olcp2l7v928qkn',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-east-1'          // e.g. us-east-2
     },
     api: {
